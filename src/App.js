8027cbfc5fd2d41dbddf9e6ddef801560d4a540c
paritosh_pundir <0p0a0r0i@gmail.com>
2017-11-11 23:12:10 +0530

    First commit with basic setup

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <section className="main-banner" style={{backgroundImage: 'url(./images/bg.svg)'}}>
+      <div className="container ">
+        <div className="row justify-content-center align-items-center d-flex full-height">
+          <div className="col-12 col-md-8 text-center">
+            <img alt="image" className="App-logo" src={logo} />
+            <h1>Score: 0</h1>
+            <div className="form-group">
+              <textarea class="form-control" rows="3"></textarea>
+            </div>
+          </div>
+        </div>
+      </div>
+      </section>
+    );
+  }
+}
+
+export default App;
