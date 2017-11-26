11031ca66a793c15d2004e6079588c0a5025e55f
paritosh_pundir <0p0a0r0i@gmail.com>
2017-11-26 09:48:46 +0530

    Analysis API route created in the node server

--- API/main.js
+++ API/main.js
@@ -2,12 +2,17 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
-const port = 3004
+const port = 8080
 const result = require('./sentialgo')
 
 app.use(bodyParser.urlencoded({ extended: true }))
-app.post('/', (req, res) => {
-  res.json(result(req.body.message))
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+app.post('/', (req, res, next) => {
+  res.send(result(req.body.message))
 })
 
-app.listen(port, () => console.log(`server is running on port ${3004}!`))
+app.listen(port, () => console.log(`server is running on port ${port}!`))
--- API/sentialgo.js
+++ API/sentialgo.js
@@ -1,8 +1,13 @@
 const sentiment = require('sentiment')
 
+/**
+ * [result description]
+ * @param  {[string]} text [description]
+ * @return {[object]}      [description]
+ */
 const result = (text) => {
   const scoreValue = sentiment(text)
-  let scoreFinal = scoreValue.comparative * 100
+  let scoreFinal = scoreValue.comparative * 500
   scoreFinal = scoreFinal.toFixed(2)
 
   return {
