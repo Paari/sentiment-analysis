d232ec1115b5b45c3e6da4d5aecb9c3540408874
paritosh_pundir <0p0a0r0i@gmail.com>
2017-11-26 09:49:31 +0530

    Hit the API and get the response on the front end react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import request from 'superagent'
+
+import Score from './Components/score'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      score: 0,
+      totalScore: 0,
+      tone: [
+        'negative',
+        'neutral',
+        'positive'
+      ]
+    }
+
+    // Bind function to this
+    this.getScore = this.getScore.bind(this)
+  }
+
+  getScore(inputMessage) {
+    // hit the api and get the score
+    request.post('http://doremember.dev')
+    .type('application/x-www-form-urlencoded')
+    .send({'message': inputMessage})
+    .end( (err, res) => {
+      this.setState({
+        score: res.body.score,
+        totalScore: res.body.totalScore
+      })
+    })
+  }
+
+  /**
+   * [whileWriting description]
+   * @param  {[text]} inputMessage [The text input from the user]
+   * @return {[object]}            [result from the server after anlysis]
+   */
+  whileWriting(inputMessage) {
+    this.getScore(inputMessage)
+  }
+
   render() {
+    /**
+     * [checks the value of overall score and gets the tone from state]
+     * @return {[string]}
+     */
+    const toneOfWords = () => {
+      if (this.state.totalScore === 0 ) {
+        return this.state.tone[1]
+      }
+      if (this.state.totalScore < 0) {
+        return this.state.tone[0]
+      }
+
+      return this.state.tone[2]
+    }
+
     return (
       <section className="main-banner" style={{backgroundImage: 'url(./images/bg.svg)'}}>
       <div className="container ">
         <div className="row justify-content-center align-items-center d-flex full-height">
           <div className="col-12 col-md-8 text-center">
-            <img alt="image" className="App-logo" src={logo} />
-            <h1 className="score-heading">Score: 0</h1>
+            <img alt="logo" className="App-logo" src={logo} />
+            <h2>Tone: {toneOfWords()}</h2>
+            <Score score={this.state.score} />
             <div className="form-group">
-              <textarea class="form-control comment-input" rows="3"></textarea>
+              <textarea
+                className="form-control comment-input"
+                onChange={(event) => this.whileWriting(event.target.value)}
+                rows="3" />
             </div>
           </div>
         </div>
       </div>
       </section>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/Components/score.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Score = (props) => {
+  return (
+    <h1 className="score-heading">Score: {props.score}</h1>
+  )
+}
+
+export default Score
